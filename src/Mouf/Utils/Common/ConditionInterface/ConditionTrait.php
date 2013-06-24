c7b4d4dbbf3aeb7a1b3320cc045d3a5f31953c35
David Négrier <d.negrier@thecodingmachine.com>
2013-06-24 18:44:05 +0200

    Commiting setter trait for ConditionInterface

---
+++ src/Mouf/Utils/Common/ConditionInterface/ConditionTrait.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+namespace Mouf\Utils\Common\ConditionInterface;
+
+/**
+ * A trait that adds a setter on a condition.
+ *
+ */
+trait ConditionTrait {
+	
+	/**
+	 * @var ConditionInterface
+	 */
+	protected $condition;
+	
+	/**
+	 * Sets the condition.
+	 * 
+	 * @Important IfSet
+	 * @param ConditionInterface $condition
+	 */
+	public function setCondition(ConditionInterface $condition = null) {
+		$this->condition = $condition;
+	}
+}
+?>
